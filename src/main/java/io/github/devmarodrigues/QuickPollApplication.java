d0a518b644b26d10015441af357ff0ec9b918330
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-01-06 14:13:55 -0300

    Project created

---
+++ src/main/java/io/github/devmarodrigues/QuickPollApplication.java
@@ -0,0 +1,13 @@
+package io.github.devmarodrigues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPollApplication.class, args);
+	}
+
+}
