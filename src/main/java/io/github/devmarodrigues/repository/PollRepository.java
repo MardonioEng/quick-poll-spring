a47944de2a71a293024343e7df701bce2e77a9bc
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-01-16 09:19:04 -0300

    PollController

--- src/main/java/io/github/devmarodrigues/repository/PollRepository.java
+++ src/main/java/io/github/devmarodrigues/repository/PollRepository.java
@@ -3,5 +3,5 @@ package io.github.devmarodrigues.repository;
 import io.github.devmarodrigues.domain.Poll;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PollReporitory extends CrudRepository<Poll, Long> {
+public interface PollRepository extends CrudRepository<Poll, Long> {
 }
